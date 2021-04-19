28d0c5c9e7b10dc1d6c1d12c4778f18c4c30ffec
gsidburycrawford <geterry12345@gmail.com>
2021-04-19 16:12:11 -0400

    Initial commit

---
+++ src/main/java/com/company/com/Main.java
@@ -0,0 +1,10 @@
+package com.company.com;
+
+public class Main {
+    public static void main(String[] args) {
+        Pet cat = new Pet("Bella", 9, "Jharna's house");
+        System.out.println(cat.getName() + " is " + cat.getAge() + " years old. ");
+        Holiday valentinesDay = new Holiday("Valentines Day",2, 14);
+        System.out.println(valentinesDay.holidayInfo());
+    }
+}
